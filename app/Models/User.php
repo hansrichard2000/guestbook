8c2977453c99f89ab6ac65d46cc52e5b08700946
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-11-23 11:56:25 +0700

    Input some code for api phase 1

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('api-register', [\App\Http\Controllers\Api\Auth\RegisterController::class, 'register']);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
